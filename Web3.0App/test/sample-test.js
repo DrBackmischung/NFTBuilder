d7390a530ca4b226b38972e14fe5308dd5bf13d3
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2022-03-01 11:17:08 +0100

    tests

--- Web3.0App/test/sample-test.js
+++ Web3.0App/test/sample-test.js
@@ -17,3 +17,28 @@ describe("Greeter", function () {
     expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });
+
+describe("NFT", function () {
+  it("Should mint and transfer an NFT to someone", async function () {
+    const Flauuuschi = await ethers.getContractFactory("Flauuuschi");
+    const flauuuschi = await Flauuuschi.deploy();
+    await flauuuschi.deployed();
+
+    const recipient = '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266';
+    const metadataURI = 'cid/test.png';
+
+    let balance = await flauuuschi.balanceOf(recipient);
+    expect(balance).to.equal(0);
+
+    const newlyMintedToken = await flauuuschi.payToMint(recipient, metadataURI, { value: ethers.utils.parseEther('0.05') });
+
+    // wait until the transaction is mined
+    await newlyMintedToken.wait();
+
+    balance = await flauuuschi.balanceOf(recipient)
+    expect(balance).to.equal(1);
+
+    expect(await flauuuschi.isContentOwned(metadataURI)).to.equal(true);
+    const newlyMintedToken2 = await flauuuschi.payToMint(recipient, 'foo', { value: ethers.utils.parseEther('0.05') });
+  });
+});
